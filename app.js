dcededf1fd72712bc94409cbd49ce571ba680b3f
Yinmm <yinmin9527@gmail.com>
2020-09-23 15:40:50 +0800

    登录优化

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
+//const os = require('os')
+// 引入body-parser模块
+const bodyParser = require('body-parser');
+
 // const compression = require('compression')
 const app = express()
 
-//下面代码需要写到静态资源托管之前
+
 // app.use(compression())
+// 配置body-parser模块
+//var networkInterfaces = os.networkInterfaces();
+//var ip = networkInterfaces.en0[1].address
+//console.log(networkInterfaces);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(history());
+app.post('/', (req, res) => {
+    // 接收请求参数
+    //console.log(JSON.stringify(req.body));
+    var value = JSON.stringify(req.body)
+    var Jsessionid = JSON.parse(value)
+    //console.log(Jsessionid.JSESSIONID)
+    //res.redirect("http://10.0.12.154:8093?JESSIONID="+Jsessionid.JSESSIONID)
+    res.redirect("http://10.0.10.246:8093?JESSIONID="+Jsessionid.JSESSIONID)
+
+})
 app.use(express.static('./dist'))
 
 
